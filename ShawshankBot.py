48da9910efd8793b4e425f105ac74bc11cc5dbee
denis <danravide@gmail.com>
2024-01-28 20:48:43 +0300

    added json configuration

--- ShawshankBot.py
+++ ShawshankBot.py
@@ -1,23 +1,27 @@
 #Обработчик команд
-
+import json
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, Message,ReplyKeyboardRemove
 from telebot import types
 TOKEN = "6555005944:AAF2jQFN2DCbwE3EjXjNzUAB5TMewTIB_YU"
 bot = telebot.TeleBot(TOKEN)
 
-
+with open("Quest.json", "r", encoding="utf-8") as f:
+    quest_data = json.load(f)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(KeyboardButton("Поехали💨"))
     bot.send_message(message.chat.id, f"💫Привет, {message.from_user.first_name}, меня зовут {bot.get_my_name().name}.💫"
                                       f"\n⚡️Ты попал в бот-квест!⚡️"
-                                      f"\n❗️Чтобы подробнее ознакомиться с ботом, напиши /help.❗️")
+                                      f"\nЧтобы начать нажмите на кнопку \"Поехали💨\""
+                                      f"\n❗️Чтобы подробнее ознакомиться с ботом, напиши /help.❗️", reply_markup=markup)
     with open("media/start_picture.jpg", "rb") as f:
         bot.send_photo(message.chat.id, f)
 
 
-markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
 
 @bot.message_handler(commands=["help"])
 def handle_help(message):
@@ -26,39 +30,79 @@ def handle_help(message):
                                       f"\n💥Суть заключается в том, что ты должен совершать верные решения от которых зависит твоя жизнь💥."
                                       f"\n🚨Малейший неверный ход и тебя опять закинут за решетку🚨. "
                                       f"\n🤫Будь осторожен и внимателен🤫"
-                                      f"\nЕсли готов к прохождению жми \"Поехали\"", reply_markup=markup)
-markup.add(KeyboardButton("Поехали💨"))
+                                      f"\nЕсли готов к прохождению жми \"Поехали💨\"")
+
 
 
 @bot.message_handler(content_types=["text"])
-
-
 def handle_questions_1_1_location(message: Message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
     if message.text == "Поехали💨":
-        bot.send_message(message.chat.id, "Тебя ховут Энди Дюфрейн, и тебе впояли два пожизненных срока за двойное убийство,"
-                                          " но сам ты толком не помнишь убивал ли ты или нет, так как был пьян..."
-                                          " Попав в тюрьму ты задумываешься о том, бежать или не бежать?"
-                                          " Это и есть твоя первая задача, но тут еще появилась загвоздка,"
-                                          " если я хочу сбежать, то как мне это сделать, побыстрее или сбежать через время разработав план..."
-                                          " Это и есть твоя первая задача...")
-"""
-        markup.add(KeyboardButton('Бежать как можно быстрее.'))
-        markup.add(KeyboardButton('Сбежать через время, продумав план.'))
-        markup.add(KeyboardButton('Остаться в тюрьме отбывать два пожизненных заключения.'))
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        button_captions = quest_data["location1_1"]["buttons"]
+        for s in button_captions:
+            button = types.KeyboardButton(s)
+            markup.add(button)
+
+        with open(quest_data["location1_1"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+
+        bot.send_message(message.chat.id, quest_data["location1_1"]["description"], reply_markup=markup)
+
+    #location 2.1 (Вариант 1)
+    elif message.text == "Бежать как можно быстрее.":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        button_captions = quest_data["location2_1"]["buttons"]
+        for s in button_captions:
+            button = types.KeyboardButton(s)
+            markup.add(button)
+        bot.send_message(message.chat.id, quest_data["location2_1"]["description"], reply_markup=markup)
 
 
-        location1_1_1question = types.KeyboardButton("Бежать как можно быстрее.")
-        location1_1_2question = types.KeyboardButton("Сбежать через время, продумав план.")
-        location1_1_3question = types.KeyboardButton("Остаться в тюрьме отбывать два пожизненных заключения.")
-        markup.add(location1_1_1question, location1_1_2question, location1_1_3question)
-"""
+        with open(quest_data["location2_1"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
 
+    #location 3.1 вариант 1 Проигрышный
+    elif message.text == "Украсть ключи и одежду":
+        with open(quest_data["location3_1"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+        bot.send_message(message.chat.id, quest_data["location3_1"]["description"])
+        bot.send_message(message.chat.id, quest_data["location3_1"]["lose_text"], reply_markup=ReplyKeyboardRemove())
 
+    #location 3.2 вариант 2 Проигрышный
+    elif message.text == "Спрататься во время прогулки":
+        with open(quest_data["location3_2"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+        bot.send_message(message.chat.id, quest_data["location3_2"]["description"])
+        bot.send_message(message.chat.id, quest_data["location3_2"]["lose_text"], reply_markup=ReplyKeyboardRemove())
 
+    #location 2.2 Вариант 2
+    elif message.text == "Сбежать через время, продумав план.":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        button_captions = quest_data["location2_2"]["buttons"]
+        for s in button_captions:
+            button = types.KeyboardButton(s)
+            markup.add(button)
+        with open(quest_data["location2_2"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+        bot.send_message(message.chat.id, quest_data["location2_2"]["description"], reply_markup=markup)
 
+    elif message.text == "Познакомиться с Рэдом":
+        with open(quest_data["location3_3"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+            bot.send_message(message.chat.id, quest_data["location3_3"]["description"])
+        bot.send_message(message.chat.id, quest_data["location3_3"]["win_text"], reply_markup=ReplyKeyboardRemove())
 
+    elif message.text == "Познакомиться с Бруксом":
+        with open(quest_data["location3_4"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+        bot.send_message(message.chat.id, quest_data["location3_4"]["description"])
+        bot.send_message(message.chat.id, quest_data["location3_4"]["lose_text"], reply_markup=ReplyKeyboardRemove())
 
+    elif message.text == "Остаться в тюрьме отбывать два пожизненных заключения.":
+        with open(quest_data["location2_3"]["picture_file_name"], "rb") as f:
+            bot.send_photo(message.chat.id, f)
+        bot.send_message(message.chat.id, quest_data["location2_3"]["description"])
+        bot.send_message(message.chat.id, quest_data["location3_4"]["lose_text"], reply_markup=ReplyKeyboardRemove())
 
 
 
