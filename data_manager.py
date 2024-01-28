2df6c3ed8af72f39d7a79238c8b8930ac9ab1753
denis <danravide@gmail.com>
2024-01-28 14:24:36 +0300

    initial commit

---
+++ data_manager.py
@@ -0,0 +1,10 @@
+#Работа с JSON
+
+
+
+"""
+@bot.message_handler(commands=['send_photo'])
+def send_photo(message):
+    with open('C:\Users\denis\PycharmProjects2.0\ShawshenlQuestBot\media Фото к проекту\start_picture', 'rb') as f:
+        bot.send_photo(message.chat.id, f)
+"""
